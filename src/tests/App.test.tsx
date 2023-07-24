cc6b664dac568f230d26f633599b6f4bb0ddb985
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 14:35:52 +0100

    Formatting

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,12 +3,12 @@
 
 //An example of using react-testing-library
 describe("App component", async () => {
-  test("Should have text Hello, World on it", () => {
-    // render(<App />);
-    // const elem = screen.getByText("Hello, World");
-    // expect(elem).toBeInTheDocument();
+    test("Should have text Hello, World on it", () => {
+        // render(<App />);
+        // const elem = screen.getByText("Hello, World");
+        // expect(elem).toBeInTheDocument();
 
-    // Temp test to pass
-    expect(1).toBe(1);
-  });
+        // Temp test to pass
+        expect(1).toBe(1);
+    });
 });
