585434a1a2ed183f9b17e908096ed61bc2741ba5
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-07-24 16:02:14 +0100

    formatted everything and added code to episodeCode.ts and episodeCode.spec.ts

---
+++ src/core/episodeCode.ts
@@ -0,0 +1,17 @@
+export function getEpisodeCode(season: number, episode: number): string {
+  let seasonCode: string;
+  if (season < 10) {
+    seasonCode = `S0${season}`;
+  } else {
+    seasonCode = `S${season}`;
+  }
+
+  let episodeCode: string;
+  if (episode < 10) {
+    episodeCode = `E0${episode}`;
+  } else {
+    episodeCode = `E${episode}`;
+  }
+
+  return seasonCode + episodeCode;
+}
