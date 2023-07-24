fe26865d0ae94589803eabd1958529c034665677
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 14:52:33 +0100

    Add example interface for episode object

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,34 @@
 import "./App.css";
 import episodes from "../data/got-episodes.json";
 
-console.log(`Imported ${episodes.length} episode(s)`);
-console.log(`First episode's name is ${episodes[0].name}`);
+interface IEpisode {
+    id: number;
+    url: string;
+    name: string;
+    season: number;
+    number: number;
+    type: string;
+    airdate: string;
+    airtime: string;
+    airstamp: string;
+    rating: { average: number };
+    runtime: number;
+    image: {
+        medium: string;
+        original: string;
+    };
+    summary: string;
+    _links: { self: { href: string } };
+}
 
 function App() {
-  return <div>Hello world</div>;
+    return (
+        <>
+            {episodes.map((episode: IEpisode) => (
+                <div key={episode.id}>{episode.name}</div>
+            ))}
+        </>
+    );
 }
 
 export default App;
