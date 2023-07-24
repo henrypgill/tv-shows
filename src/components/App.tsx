6316fddbbc4017148c637b0f5ba3a06341bccfeb
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 14:58:18 +0100

    Formatting to 2 tabWidth

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,33 +2,33 @@ import "./App.css";
 import episodes from "../data/got-episodes.json";
 
 interface IEpisode {
-    id: number;
-    url: string;
-    name: string;
-    season: number;
-    number: number;
-    type: string;
-    airdate: string;
-    airtime: string;
-    airstamp: string;
-    rating: { average: number };
-    runtime: number;
-    image: {
-        medium: string;
-        original: string;
-    };
-    summary: string;
-    _links: { self: { href: string } };
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  rating: { average: number };
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
 }
 
 function App() {
-    return (
-        <>
-            {episodes.map((episode: IEpisode) => (
-                <div key={episode.id}>{episode.name}</div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {episodes.map((episode: IEpisode) => (
+        <div key={episode.id}>{episode.name}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
