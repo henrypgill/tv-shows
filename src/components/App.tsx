7c8b29aaf0369f4e282f9d3a199e2661d1ae76ce
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 14:20:42 +0100

    Removed example component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,7 @@
-import { MyComponent } from "./MyComponent";
 import "./App.css";
-import { greet } from "../greet";
 
 function App() {
-    return (
-        <div className="App">
-            <MyComponent />
-
-            {greet("World")}
-        </div>
-    );
+  return <div>Hello world</div>;
 }
 
 export default App;
