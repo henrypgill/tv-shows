f5bfe734d0de3a0b1565e83d975e12ffbc397074
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-24 14:47:12 +0100

    Import JSON episode data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import episodes from "../data/got-episodes.json";
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
 
 function App() {
-    return <div>Hello world</div>;
+  return <div>Hello world</div>;
 }
 
 export default App;
