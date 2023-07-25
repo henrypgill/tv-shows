b76c9e87f84b4aee045c8553945e01e0dc83e268
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-25 10:54:37 +0100

    Use new filtering function and added state for controlled input

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import episodes from "../data/got-episodes.json";
 import { EpisodeList } from "./EpisodeList";
+import { searchNameOrSummary } from "../core/episodeFilter";
+import { useState } from "react";
 
 function App() {
-  return <EpisodeList episodes={episodes} />;
+  //@ts-ignore
+  const [textInput, setTextInput] = useState("");
+
+  const filteredEpisodes = episodes.filter(searchNameOrSummary(textInput));
+
+  return <EpisodeList episodes={filteredEpisodes} />;
 }
 
 export default App;
