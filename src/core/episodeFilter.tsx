d050f02c82cfd6478a4f5b72aac2da7731b6e8e4
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-25 10:54:21 +0100

    Updated filterNameAndSummary to return callback for cleaner filtering

--- src/core/episodeFilter.tsx
+++ src/core/episodeFilter.tsx
@@ -1,12 +1,13 @@
 import { IEpisode } from "../components/EpisodeView";
 
-export function filterNameAndSummary(
-  episode: IEpisode,
+export function searchNameOrSummary(
   subStr: string
-): boolean {
-  const searchStr = subStr.toLowerCase();
-  const name = episode.name.toLowerCase();
-  const summary = episode.summary.toLowerCase();
+): (episode: IEpisode) => boolean {
+  return (episode) => {
+    const searchStr = subStr.toLowerCase();
+    const name = episode.name.toLowerCase();
+    const summary = episode.summary.toLowerCase();
 
-  return name.includes(searchStr) || summary.includes(searchStr);
+    return name.includes(searchStr) || summary.includes(searchStr);
+  };
 }
--- src/tests/episodeFilter.spec.ts
+++ src/tests/episodeFilter.spec.ts
@@ -1,18 +1,18 @@
-import { filterNameAndSummary } from "../core/episodeFilter";
+import { searchNameOrSummary } from "../core/episodeFilter";
 import episodes from "../data/got-episodes.json";
 
 // Testing specifically game of thrones episodes from local JSON data
 // TODO: Update episode data when we move to using fetch
 
 test("test search string found in name", () => {
-  const result = episodes.filter((e) => filterNameAndSummary(e, "Kingsroad"));
+  const result = episodes.filter(searchNameOrSummary("Kingsroad"));
 
   expect(result.length).toBe(1);
   expect(result[0].id).toBe(4953);
 });
 
 test("test search string found in summary", () => {
-  const result = episodes.filter((e) => filterNameAndSummary(e, "Hound"));
+  const result = episodes.filter(searchNameOrSummary("Hound"));
 
   expect(result.length).toBe(2);
 
