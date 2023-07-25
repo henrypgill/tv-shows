b216b3936ad82006ba9310f30c9c26ac832c200b
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-25 13:49:45 +0100

    removed Header component in favor of simplifying the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,24 +3,25 @@ import episodes from "../data/got-episodes.json";
 import { EpisodeList } from "./EpisodeList";
 import { searchNameOrSummary } from "../core/episodeFilter";
 import { useState } from "react";
+import { SearchBox } from "./SearchBox";
 import Footer from "./Footer";
-import Header from "./Header";
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
+
   const filteredEpisodes = episodes.filter(searchNameOrSummary(searchInput));
 
   return (
     <>
-      <Header
-        searchInput={searchInput}
-        onChangeHandler={setSearchInput}
-        filteredCount={filteredEpisodes.length}
-        totalCount={episodes.length}
-      />
+      <header>
+        <SearchBox searchInput={searchInput} onChangeHandler={setSearchInput} />
+        <h3>{`Displaying ${filteredEpisodes.length}/${episodes.length} Episodes`}</h3>
+      </header>
+
       <main>
         <EpisodeList episodes={filteredEpisodes} />
       </main>
+
       <Footer />
     </>
   );
