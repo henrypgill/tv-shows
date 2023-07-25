a0f1f17a159a581755654a6b8b349acca30dc700
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-07-25 15:16:43 +0100

    added tests for episode filter function

--- src/tests/episodeFilter.spec.ts
+++ src/tests/episodeFilter.spec.ts
@@ -1,4 +1,5 @@
-import { searchNameOrSummary } from "../core/episodeFilter";
+
+import { searchNameOrSummary, searchEpisodeId } from "../core/episodeFilter";
 import episodes from "../data/got-episodes.json";
 
 // Testing specifically game of thrones episodes from local JSON data
@@ -18,4 +19,15 @@ test("test search string found in summary", () => {
 
   expect(result[0].id).toBe(4976);
   expect(result[1].id).toBe(4984);
+});
+
+test("test search episode ", () => {
+  const result = episodes.filter(searchEpisodeId("4956"));
+  expect(result[0].id).toBe(4956);
+});
+
+test("test search show all episodes", () => {
+  const result = episodes.filter(searchEpisodeId(""));
+  expect(result.length).toBe(73);
+
 });
