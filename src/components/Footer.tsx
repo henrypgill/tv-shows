6ff06e0519e2544c7f35d6f123243823ed447b2d
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-07-25 09:50:02 +0100

    created footer component with link to data source and accreditation to it as well

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(): JSX.Element {
+
+
+    return <footer>
+        <p className="footer-link">All information displayed on this page is obtained from <a href="https://www.TVMaze.com">TVMaze.com</a>.</p>
+    </footer>
+}
+
+export default Footer
