4895307ae503f30026e0d4b6554d854caccfda86
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-07-25 09:52:09 +0100

    formatted files from previous commit to pass CI checks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
 function Footer(): JSX.Element {
-
-
-    return <footer>
-        <p className="footer-link">All information displayed on this page is obtained from <a href="https://www.TVMaze.com">TVMaze.com</a>.</p>
+  return (
+    <footer>
+      <p className="footer-link">
+        All information displayed on this page is obtained from{" "}
+        <a href="https://www.TVMaze.com">TVMaze.com</a>.
+      </p>
     </footer>
+  );
 }
 
-export default Footer
+export default Footer;
