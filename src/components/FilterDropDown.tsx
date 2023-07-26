6bd448ad2254987e6e234b7ce83804c2b593480e
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-26 15:35:12 +0100

    Refactor FilterDropDown component for conditional rendering of shows OR episodes

--- src/components/FilterDropDown.tsx
+++ src/components/FilterDropDown.tsx
@@ -1,6 +1,6 @@
 import { IEpisode } from "../core/fetchEpisodes";
-import { getEpisodeCode } from "../core/episodeCode";
 import { IShow } from "../core/fetchShows";
+import { getEpisodeCode } from "../core/episodeCode";
 
 type FilterDropProps = {
   dropDownState: string;
@@ -23,43 +23,43 @@ export default function FilterDropDown({
   dropDownState,
   handleOnChange,
 }: FilterDropProps): JSX.Element {
-  if (type === "episode") {
-    const optionsElements = options.map((e) => (
-      <option key={e.id} value={e.id}>
-        {getEpisodeCode(e.season, e.number)}
+  return (
+    <select
+      onChange={(e) => handleOnChange(e.target.value)}
+      value={dropDownState}
+      className="dropdown-input"
+    >
+      {type === "show" ? getShowOptions(options) : getEpisodeOptions(options)}
+    </select>
+  );
+}
+
+function getEpisodeOptions(options: IEpisode[]): JSX.Element {
+  return (
+    <>
+      <option value="">All Episodes</option>
+      {options.map((e) => (
+        <option key={e.id} value={e.id}>
+          {getEpisodeCode(e.season, e.number)}
+        </option>
+      ))}
+    </>
+  );
+}
+
+function getShowOptions(options: IShow[]): JSX.Element {
+  return (
+    <>
+      <option disabled value="">
+        Select Show
       </option>
-    ));
-
-    return (
-      <select
-        onChange={(e) => handleOnChange(e.target.value)}
-        value={dropDownState}
-        className="dropdown-input"
-      >
-        <option value="">All Episodes</option>
-        {optionsElements}
-      </select>
-    );
-  } else {
-    const optionsElements = options
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map((s) => (
-        <option key={s.id} value={s.id}>
-          {s.name}
-        </option>
-      ));
-
-    return (
-      <select
-        onChange={(e) => handleOnChange(e.target.value)}
-        value={dropDownState}
-        className="dropdown-input"
-      >
-        <option disabled value="">
-          Select Show
-        </option>
-        {optionsElements}
-      </select>
-    );
-  }
+      {options
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((s) => (
+          <option key={s.id} value={s.id}>
+            {s.name}
+          </option>
+        ))}
+    </>
+  );
 }
