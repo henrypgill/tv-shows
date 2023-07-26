4f413df4ce78f935a04ef385a8a8a04bc338325d
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-26 15:33:53 +0100

    Moved data dir into tests as it's only used for testing now

--- src/tests/fetchData.spec.ts
+++ src/tests/fetchData.spec.ts
@@ -1,8 +1,5 @@
-import episodes from "../data/got-episodes.json";
+import episodes from "./data/got-episodes.json";
 import { getEpisodes } from "../core/fetchEpisodes";
-
-// Testing specifically game of thrones episodes from local JSON data
-// TODO: Update episode data when we move to using fetch
 
 test("test fetch data is correct as of 26/07/2023 at 13:46:48 pm", async () => {
   const fetchedEpisodes = await getEpisodes("82");
